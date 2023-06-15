0392be7edbba0bc3535c2c126ffb89275c668092
Felix Le <31028775+felixlephuoc@users.noreply.github.com>
2023-06-15 17:13:58 +0700

    Edit prepare_data.py to process multiple frames using multiprocessing

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -6,6 +6,7 @@ from functools import partial
 from multiprocessing.sharedctypes import RawValue
 from PIL import Image
 from tqdm import tqdm
+from glob import glob
 from torchvision.transforms import functional as trans_fn
 import os
 from pathlib import Path
@@ -67,29 +68,40 @@ class WorkingContext():
         with self.counter_lock:
             return self.counter.value
 
-def prepare_process_worker(wctx, file_subset):
-    for file in file_subset:
-        i, imgs = wctx.resize_fn(file)
-        lr_img, hr_img, sr_img = imgs
-        if not wctx.lmdb_save:
-            lr_img.save(
-                '{}/lr_{}/{}.png'.format(wctx.out_path, wctx.sizes[0], i.zfill(5)))
-            hr_img.save(
-                '{}/hr_{}/{}.png'.format(wctx.out_path, wctx.sizes[1], i.zfill(5)))
-            sr_img.save(
-                '{}/sr_{}_{}/{}.png'.format(wctx.out_path, wctx.sizes[0], wctx.sizes[1], i.zfill(5)))
-        else:
-            with wctx.env.begin(write=True) as txn:
-                txn.put('lr_{}_{}'.format(
-                    wctx.sizes[0], i.zfill(5)).encode('utf-8'), lr_img)
-                txn.put('hr_{}_{}'.format(
-                    wctx.sizes[1], i.zfill(5)).encode('utf-8'), hr_img)
-                txn.put('sr_{}_{}_{}'.format(
-                    wctx.sizes[0], wctx.sizes[1], i.zfill(5)).encode('utf-8'), sr_img)
-        curr_total = wctx.inc_get()
-        if wctx.lmdb_save:
-            with wctx.env.begin(write=True) as txn:
-                txn.put('length'.encode('utf-8'), str(curr_total).encode('utf-8'))
+def prepare_process_worker(wctx, videos_path_subset):
+    for video_path in videos_path_subset:
+        video_id = os.path.basename(video_path)
+        #for file in glob(os.path.join(video_path, '*.jpg')):
+        for file in Path(video_path).rglob('*.jpg'):
+            if not file.is_file():
+                continue
+            i, imgs = wctx.resize_fn(file)
+            lr_img, hr_img, sr_img = imgs
+            
+            # create the output directory
+            os.makedirs('{}/lr_{}/{}'.format(wctx.out_path, sizes[0], video_id), exist_ok=True)
+            os.makedirs('{}/hr_{}/{}'.format(wctx.out_path, sizes[1], video_id), exist_ok=True)
+            os.makedirs('{}/sr_{}_{}/{}'.format(wctx.out_path, sizes[0], sizes[1], video_id), exist_ok=True)
+            
+            if not wctx.lmdb_save:
+                lr_img.save(
+                    '{}/lr_{}/{}/{}.png'.format(wctx.out_path, wctx.sizes[0],video_id, i.zfill(5)))
+                hr_img.save(
+                    '{}/hr_{}/{}/{}.png'.format(wctx.out_path, wctx.sizes[1],video_id, i.zfill(5)))
+                sr_img.save(
+                    '{}/sr_{}_{}/{}/{}.png'.format(wctx.out_path, wctx.sizes[0], wctx.sizes[1],video_id, i.zfill(5)))
+            else:
+                with wctx.env.begin(write=True) as txn:
+                    txn.put('lr_{}_{}'.format(
+                        wctx.sizes[0], i.zfill(5)).encode('utf-8'), lr_img)
+                    txn.put('hr_{}_{}'.format(
+                        wctx.sizes[1], i.zfill(5)).encode('utf-8'), hr_img)
+                    txn.put('sr_{}_{}_{}'.format(
+                        wctx.sizes[0], wctx.sizes[1], i.zfill(5)).encode('utf-8'), sr_img)
+            curr_total = wctx.inc_get()
+            if wctx.lmdb_save:
+                with wctx.env.begin(write=True) as txn:
+                    txn.put('length'.encode('utf-8'), str(curr_total).encode('utf-8'))
 
 def all_threads_inactive(worker_threads):
     for thread in worker_threads:
@@ -97,13 +109,16 @@ def all_threads_inactive(worker_threads):
             return False
     return True
 
-def prepare(img_path, out_path, n_worker, sizes=(16, 128), resample=Image.BICUBIC, lmdb_save=False):
+def prepare(frame_path, out_path, n_worker, n_frame, sizes=(16, 128), resample=Image.BICUBIC, lmdb_save=False):
+    
     resize_fn = partial(resize_worker, sizes=sizes,
                         resample=resample, lmdb_save=lmdb_save)
-    files = [p for p in Path(
-        '{}'.format(img_path)).glob(f'**/*')]
+    
+    videos_path = [os.path.join(frame_path, video_id) for video_id in os.listdir(frame_path) if os.path.isdir(os.path.join(frame_path, video_id))]
+    #files = [p for p in Path(
+    #    '{}'.format(img_path)).glob(f'**/*')]
 
-    if not lmdb_save:
+    if not lmdb_save: 
         os.makedirs(out_path, exist_ok=True)
         os.makedirs('{}/lr_{}'.format(out_path, sizes[0]), exist_ok=True)
         os.makedirs('{}/hr_{}'.format(out_path, sizes[1]), exist_ok=True)
@@ -118,50 +133,62 @@ def prepare(img_path, out_path, n_worker, sizes=(16, 128), resample=Image.BICUBI
         if lmdb_save:
             multi_env = env
 
-        file_subsets = np.array_split(files, n_worker)
+        videos_path_subsets = np.array_split(videos_path, n_worker)
         worker_threads = []
         wctx = WorkingContext(resize_fn, lmdb_save, out_path, multi_env, sizes)
 
         # start worker processes, monitor results
         for i in range(n_worker):
-            proc = Process(target=prepare_process_worker, args=(wctx, file_subsets[i]))
+            proc = Process(target=prepare_process_worker, args=(wctx, videos_path_subsets[i]))
             proc.start()
             worker_threads.append(proc)
         
-        total_count = str(len(files))
+        total_videos_path = len(videos_path)
+        total_num_frames = int(total_videos_path * n_frame)
         while not all_threads_inactive(worker_threads):
-            print("\r{}/{} images processed".format(wctx.value(), total_count), end=" ")
+            print("\r {}/{} frames processed".format(wctx.value(), total_num_frames), end=" ")
             time.sleep(0.1)
 
-    else:
+    else:  
         total = 0
-        for file in tqdm(files):
-            i, imgs = resize_fn(file)
-            lr_img, hr_img, sr_img = imgs
-            if not lmdb_save:
-                lr_img.save(
-                    '{}/lr_{}/{}.png'.format(out_path, sizes[0], i.zfill(5)))
-                hr_img.save(
-                    '{}/hr_{}/{}.png'.format(out_path, sizes[1], i.zfill(5)))
-                sr_img.save(
-                    '{}/sr_{}_{}/{}.png'.format(out_path, sizes[0], sizes[1], i.zfill(5)))
-            else:
-                with env.begin(write=True) as txn:
-                    txn.put('lr_{}_{}'.format(
-                        sizes[0], i.zfill(5)).encode('utf-8'), lr_img)
-                    txn.put('hr_{}_{}'.format(
-                        sizes[1], i.zfill(5)).encode('utf-8'), hr_img)
-                    txn.put('sr_{}_{}_{}'.format(
-                        sizes[0], sizes[1], i.zfill(5)).encode('utf-8'), sr_img)
-            total += 1
-            if lmdb_save:
-                with env.begin(write=True) as txn:
-                    txn.put('length'.encode('utf-8'), str(total).encode('utf-8'))
+        for video_path in videos_path:
+            video_id = os.path.basename(video_path)
+            for file in Path(video_path).rglob('*.jpg'):
+                if not file.is_file():
+                    continue
+                i, imgs = resize_fn(file)
+                lr_img, hr_img, sr_img = imgs
+                
+                # create the output directory
+                os.makedirs('{}/lr_{}/{}'.format(out_path, sizes[0], video_id), exist_ok=True)
+                os.makedirs('{}/hr_{}/{}'.format(out_path, sizes[1], video_id), exist_ok=True)
+                os.makedirs('{}/sr_{}_{}/{}'.format(out_path, sizes[0], sizes[1], video_id), exist_ok=True)
+                    
+                if not lmdb_save:
+                    lr_img.save(
+                        '{}/lr_{}/{}/{}.png'.format(out_path, sizes[0],video_id, i.zfill(5)))
+                    hr_img.save(
+                        '{}/hr_{}/{}/{}.png'.format(out_path, sizes[1], video_id, i.zfill(5)))
+                    sr_img.save(
+                        '{}/sr_{}_{}/{}/{}.png'.format(out_path, sizes[0], sizes[1], video_id, i.zfill(5)))
+                else:
+                    with env.begin(write=True) as txn:
+                        txn.put('lr_{}_{}'.format(
+                            sizes[0], i.zfill(5)).encode('utf-8'), lr_img)
+                        txn.put('hr_{}_{}'.format(
+                            sizes[1], i.zfill(5)).encode('utf-8'), hr_img)
+                        txn.put('sr_{}_{}_{}'.format(
+                            sizes[0], sizes[1], i.zfill(5)).encode('utf-8'), sr_img)
+                total += 1
+                if lmdb_save:
+                    with env.begin(write=True) as txn:
+                        txn.put('length'.encode('utf-8'), str(total).encode('utf-8'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', '-p', type=str,
-                        default='{}/Dataset/celebahq_256'.format(Path.home()))
+    parser.add_argument('--frames_path', type=str,
+                        default='sample_frames',help="Path to the video_id directories containing extracted frames")
+    parser.add_argument('--n_frames', type=int, default=3, help="Number of frames extracted from each video")
     parser.add_argument('--out', '-o', type=str,
                         default='./dataset/celebahq')
 
@@ -178,5 +205,5 @@ if __name__ == '__main__':
     sizes = [int(s.strip()) for s in args.size.split(',')]
 
     args.out = '{}_{}_{}'.format(args.out, sizes[0], sizes[1])
-    prepare(args.path, args.out, args.n_worker,
+    prepare(args.frames_path, args.out, args.n_worker, args.n_frames,
             sizes=sizes, resample=resample, lmdb_save=args.lmdb)
